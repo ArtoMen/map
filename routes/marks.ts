67b3bb2716e0bc9e31897d910b714a1c406ee3d0
ReeN262 <korobk.vitaly@gmail.com>
2021-07-19 19:36:26 +0300

    test

--- routes/marks.ts
+++ routes/marks.ts
@@ -13,7 +13,7 @@ markRouter.post(
   auth,
   body('title', 'Field minimum length 1 and maximum 1000')
     .isLength({min: 1, max: 80}),
-  body('content', 'Field minimum length 1 and maximum 1000')
+  body('content', 'Field minimum length 2 and maximum 1000')
     .isLength({min: 1, max: 1000}),
   body('coordinates', 'Field is empty').notEmpty(),
   validator,
